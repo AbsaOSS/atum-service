a11095f77e67b5a45564f635488b47b680cee1d7
Daniel Kavan <dk1844@gmail.com>
2022-02-11 09:33:57 +0100

    #5 Some small PRreview fixes 2

--- src/main/scala/za/co/absa/atum/web/model/Measurement.scala
+++ src/main/scala/za/co/absa/atum/web/model/Measurement.scala
@@ -16,10 +16,7 @@
 
 package za.co.absa.atum.web.model
 
-case class Measurement
-(
-  controlName: String,
-  controlType: String,
-  controlCol: String,
-  controlValue: Any
-)
+case class Measurement(controlName: String,
+                       controlType: String,
+                       controlCol: String,
+                       controlValue: Any)
