e121e2b5a1bf7553b8ab18206232341fb524bc2c
Hector Perez <hector.perez@absa.africa>
2023-06-19 10:00:25 +0200

    minor

--- agent/src/main/scala/za/co/absa/atum/agent/AtumAgent.scala
+++ agent/src/main/scala/za/co/absa/atum/agent/AtumAgent.scala
@@ -6,6 +6,7 @@ import za.co.absa.atum.agent.model.Measurement
  *  Place holder for the agent that communicate with the API.
  */
 object AtumAgent {
+
   def measurePublish(measure: Measurement): Unit =
     println("Enqueued measurement: " + (measure))
 
