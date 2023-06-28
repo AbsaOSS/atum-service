ce4c6e89da11bb16d47e3c11e18e638c364eb71a
Hector Perez <hector.perez@absa.africa>
2023-06-28 12:51:25 +0100

    format

--- agent/src/main/scala/za/co/absa/atum/agent/AtumAgent.scala
+++ agent/src/main/scala/za/co/absa/atum/agent/AtumAgent.scala
@@ -8,30 +8,29 @@ import za.co.absa.atum.agent.model.{AtumPartitions, MeasureResult}
 object AtumAgent {
 
   /**
-   * Sends the `MeasureResult` and extra data from a given `AtumContext` to the AtumService API.
-   * @param checkpointKey
-   * @param atumContext
-   * @param measureResult
+   *  Sends the `MeasureResult` and extra data from a given `AtumContext` to the AtumService API.
+   *  @param checkpointKey
+   *  @param atumContext
+   *  @param measureResult
    */
   def publish(checkpointKey: String, atumContext: AtumContext, measureResult: MeasureResult): Unit = println(
     Seq(checkpointKey, atumContext, measureResult).mkString(" || ")
   )
 
   /**
-   * Sends a single `MeasureResult` to the AtumService API. With not AtumContext involve.
+   *  Sends a single `MeasureResult` to the AtumService API. With not AtumContext involve.
    *
-   * @param checkpointKey
-   * @param context
-   * @param measureResult
+   *  @param checkpointKey
+   *  @param context
+   *  @param measureResult
    */
   def measurePublish(checkpointKey: String, measure: MeasureResult): Unit =
     println(s"Enqueued measurement: $checkpointKey, " + (measure))
 
-
   /**
-   * Provides an AtumContext given a `AtumPartitions` instance. Retrieves the data from Atum Service API.
-   * @param atumPartitions
-   * @return
+   *  Provides an AtumContext given a `AtumPartitions` instance. Retrieves the data from Atum Service API.
+   *  @param atumPartitions
+   *  @return
    */
   def createAtumContext(atumPartitions: AtumPartitions): AtumContext = {
     AtumContext()
