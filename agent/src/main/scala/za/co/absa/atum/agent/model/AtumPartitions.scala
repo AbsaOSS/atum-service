f15d728ffa7099640c3a72ac3163b6db0da4365d
Hector Perez <hector.perez@absa.africa>
2023-06-28 13:38:17 +0100

    naming

--- agent/src/main/scala/za/co/absa/atum/agent/model/AtumPartitions.scala
+++ agent/src/main/scala/za/co/absa/atum/agent/model/AtumPartitions.scala
@@ -5,26 +5,44 @@ import za.co.absa.atum.agent.model.AtumPartitions.Partitions
 import java.time.LocalDate
 import scala.collection.immutable.ListMap
 
-case class AtumPartitions(name: String, reportDate: LocalDate, metadata: Partitions) {
+case class AtumPartitions(name: String, reportDate: LocalDate, partitions: Partitions) {
 
   /**
-   *  Creates a new Segmentation instance with the given metadata
-   *  @param metadata metadata key value map that preserves the order of arrival of the elements.
+   *  Creates a new Partition instance with the given metadata
+   *  @param partitions metadata key value map that preserves the order of arrival of the elements.
    *  @return
    */
-  def withPartitions(metadata: Partitions): AtumPartitions = this.copy(metadata = metadata)
+  def withPartitions(partitions: Partitions): AtumPartitions = this.copy(partitions = partitions)
 
   /**
-   *  Creates a new Segmentation instance with new metadata key values added known as a sub-segmentation
-   *  @param metadata metadata key value map to be added to the existing metadata
+   *  Creates a new Partition instance with new metadata key values added known as a sub-Partition
+   *  @param partitions metadata key value map to be added to the existing metadata
    *  @return
    */
-  def addPartitions(metadata: Partitions): AtumPartitions = this.copy(metadata = this.metadata ++ metadata)
+  def addPartitions(partitions: Partitions): AtumPartitions = this.copy(partitions = this.partitions ++ partitions)
+
+  /**
+   *  Creates a new Partition instance with new metadata key values added known as a sub-Partition
+   *
+   *  @param partition metadata key value map to be added to the existing metadata
+   *  @return
+   */
+  def addPartition(partition: Map[String, String]): AtumPartitions =
+    this.copy(partitions = this.partitions ++ partition)
+
+  /**
+   *  Creates a new Partition instance with new partition key values added known as a sub-Partition
+   *  @param key new partition key
+   *  @param value new partition value
+   *  @return
+   */
+  def addPartition(key: String, value: String): AtumPartitions =
+    this.copy(partitions = this.partitions + (key -> value))
 
   /**
    *  An alias for `addPartitions`
    */
-  def subPartitions(metadata: Partitions): AtumPartitions = addPartitions(metadata)
+  def subPartitions(partitions: Partitions): AtumPartitions = addPartitions(partitions)
 
 }
 
