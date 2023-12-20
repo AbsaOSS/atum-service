904b05a46d0a9f9cb5ab1d8ec6f792116930a45e
Ladislav Sulak <laco.sulak@gmail.com>
2023-12-20 12:44:19 +0100

    #117: keeping as originally to have as least change as possible in this PR

--- server/src/future/scala/za/co/absa/atum/server/future/model/Flow.scala
+++ server/src/future/scala/za/co/absa/atum/server/future/model/Flow.scala
@@ -19,10 +19,10 @@ package za.co.absa.atum.server.future.model
 import java.util.UUID
 
 case class Flow(
-                 id: Option[UUID],
-                 description: Option[String],
-                 properties: Map[String, String] = Map.empty
-               ) extends BaseApiModel {
+  id: Option[UUID],
+  description: Option[String],
+  properties: Map[String, String] = Map.empty
+) extends BaseApiModel {
 
   def apply(desc: Option[String] = None): Flow = Flow(None, desc)
 
