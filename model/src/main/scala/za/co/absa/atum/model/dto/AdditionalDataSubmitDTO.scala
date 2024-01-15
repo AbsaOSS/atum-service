58a3c52b1d919ced729bf11692f1d99396b199ee
TebaleloS <107194332+TebaleloS@users.noreply.github.com>
2024-01-15 11:33:23 +0200

    Update model/src/main/scala/za/co/absa/atum/model/dto/AdditionalDataSubmitDTO.scala
    Co-authored-by: Ladislav Sulak <laco.sulak@gmail.com>

--- model/src/main/scala/za/co/absa/atum/model/dto/AdditionalDataSubmitDTO.scala
+++ model/src/main/scala/za/co/absa/atum/model/dto/AdditionalDataSubmitDTO.scala
@@ -1,6 +1,6 @@
 package za.co.absa.atum.model.dto
 
 case class AdditionalDataSubmitDTO (
-  atumPartitioning: PartitioningDTO,
+  partitioning: PartitioningDTO,
   additionalData: Map[String, Option[String]]
 )
