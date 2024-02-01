d85c85e73c5a5f4f488ddb25323cacaa7cc1cd07
Ladislav Sulak <laco.sulak@gmail.com>
2024-02-01 17:44:55 +0100

    post-merge fixes

--- server/src/test/scala/za/co/absa/atum/server/api/TestData.scala
+++ server/src/test/scala/za/co/absa/atum/server/api/TestData.scala
@@ -41,7 +41,7 @@ trait TestData {
     partitioning = Seq.empty,
     processStartTime = ZonedDateTime.now(),
     processEndTime = None,
-    measurements = Seq.empty
+    measurements = Set.empty
   )
   protected val checkpointDTO2: CheckpointDTO = checkpointDTO1.copy(id = UUID.randomUUID())
 
