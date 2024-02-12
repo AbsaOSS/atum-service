67b1d148ca5db2c9da36d6a4435efcf5ac72e904
Ladislav Sulak <laco.sulak@gmail.com>
2024-02-12 15:23:08 +0100

    #69: fixing DB unit tests

--- database/src/test/scala/za/co/absa/atum/database/runs/WriteCheckpointTest.scala
+++ database/src/test/scala/za/co/absa/atum/database/runs/WriteCheckpointTest.scala
@@ -30,7 +30,7 @@ class WriteCheckpointTest extends DBTestSuite {
       |{
       |  "version": 1,
       |  "keys": ["key1", "key3", "key2", "key4"],
-      |  "values": {
+      |  "keysToValues": {
       |    "key1": "valueX",
       |    "key2": "valueY",
       |    "key3": "valueZ",
