19ae3739e4a51975f7e7d37be270bbfb5737ea05
AB019TC <tebalelo.sekhula@absa.africa>
2024-02-23 12:33:59 +0200

    Fixes #137 - implemented the test cases

--- database/src/test/scala/za/co/absa/atum/database/runs/GetPartitioningMeasuresTest.scala
+++ database/src/test/scala/za/co/absa/atum/database/runs/GetPartitioningMeasuresTest.scala
@@ -18,27 +18,39 @@ package za.co.absa.atum.database.runs
 
 import za.co.absa.balta.DBTestSuite
 import za.co.absa.balta.classes.JsonBString
+import za.co.absa.balta.classes.setter.CustomDBType
 
-
-class GetPartitioningMeasuresTest extends DBTestSuite{
-
+class GetPartitioningMeasuresTest extends DBTestSuite {
   private val fncGetPartitioningMeasures = "runs.get_partitioning_measures"
 
   test("Get partitioning measures should return partitioning measures for partitioning with measures") {
     val partitioning = JsonBString(
       """
         |{
-        |  "keys": [
-        |    "key1",
-        |    "key2"
-        |  ],
         |  "version": 1,
+        |  "keys": ["key1", "key3", "key2", "key4"],
         |  "keysToValues": {
-        |    "key1": "value1",
-        |    "key2": "value2"
+        |    "key1": "valueX",
+        |    "key2": "valueY",
+        |    "key3": "valueZ",
+        |    "key4": "valueA"
         |  }
         |}
         |""".stripMargin
+    )
+
+    table("runs.partitionings").insert(
+      add("partitioning", partitioning)
+        .add("created_by", "Thomas")
+    )
+
+    val fkPartitioning: Long = table("runs.partitionings").fieldValue("partitioning", partitioning, "id_partitioning").get.get
+
+    table("runs.measure_definitions").insert(
+      add("fk_partitioning", fkPartitioning)
+        .add("created_by", "Thomas")
+        .add("measure_name", s"measure${fkPartitioning}")
+        .add("measured_columns", CustomDBType("""{"col1"}""", "TEXT[]"))
     )
 
     function(fncGetPartitioningMeasures)
@@ -48,6 +60,12 @@ class GetPartitioningMeasuresTest extends DBTestSuite{
         assert(results.getInt("status").contains(11))
         assert(results.getString("status_text").contains("OK"))
       }
+
+    table("runs.measure_definitions").where(add("fk_partitioning", fkPartitioning)) { partitioningMeasuresResult =>
+      assert(partitioningMeasuresResult.hasNext)
+      val row = partitioningMeasuresResult.next()
+      assert(row.getString("created_by").contains("Thomas"))
+    }
   }
 
   test("Get partitioning measures should return error on non existing partitioning") {
@@ -77,22 +95,29 @@ class GetPartitioningMeasuresTest extends DBTestSuite{
       """
         |{
         |  "keys": [
-        |    "key1",
-        |    "key3",
-        |    "key5"
+        |    "keyA",
+        |    "keyB",
+        |    "keyC"
         |  ],
         |  "version": 1,
         |  "keysToValues": {
-        |    "key1": "value1",
-        |    "key3": "value3",
-        |    "key5": "value5"
+        |    "keyA": "valueA",
+        |    "keyB": "valueB",
+        |    "keyC": "valueC"
         |  }
         |}
         |""".stripMargin
     )
 
+    table("runs.partitionings").insert(
+      add("partitioning", partitioning)
+        .add("created_by", "Thomas")
+    )
+
+    val fkPartitioning: Long = table("runs.partitionings").fieldValue("partitioning", partitioning, "id_partitioning").get.get
+
     function(fncGetPartitioningMeasures)
-      .setParam("i_partitioning", partitioning)
+      .setParam("i_partitioning", fkPartitioning)
       .execute { queryResult =>
         val results = queryResult.distinct.next()
         assert(results.getInt("status").contains(42))
@@ -101,3 +126,4 @@ class GetPartitioningMeasuresTest extends DBTestSuite{
   }
 
 }
+
