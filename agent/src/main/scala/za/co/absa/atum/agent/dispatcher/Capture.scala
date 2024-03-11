ca43b0f91090a318637ae7db4e0393d4c7afd0bd
Filip Horňák <filip.hornak@absa.africa>
2024-03-11 11:27:27 +0100

    refactor (agent): Add Iterable Capture dispatcher

--- agent/src/main/scala/za/co/absa/atum/agent/dispatcher/Capture.scala
+++ agent/src/main/scala/za/co/absa/atum/agent/dispatcher/Capture.scala
@@ -1,24 +1,23 @@
 package za.co.absa.atum.agent.dispatcher
 
-import za.co.absa.atum.model.dto.{AdditionalDataSubmitDTO, AtumContextDTO, CheckpointDTO, PartitioningDTO, PartitioningSubmitDTO}
+import za.co.absa.atum.model.dto._
 
 import java.util
 
 class Capture(maxEvents: Int) extends Dispatcher {
   import Capture._
-  private val capture: Map[PartitioningDTO, Events] = Map.empty
 
-  private val ts = new util.TreeMap[String, Events]()
-
+  private val ts = util.Collections.synchronizedSortedMap(new util.TreeMap[String, Events]())
 
   private def createPath(partitioning: PartitioningDTO): String =
     partitioning.map(p => s"${p.key}=${p.value}").mkString(start = "/", sep = "/", end = "/")
+
   /**
-   * This method is used to ensure the server knows the given partitioning.
-   * As a response the `AtumContext` is fetched from the server.
+   *  This method is used to ensure the server knows the given partitioning.
+   *  As a response the `AtumContext` is fetched from the server.
    *
-   * @param partitioning  : PartitioningSubmitDTO to be used to ensure server knows the given partitioning.
-   * @return AtumContextDTO.
+   *  @param partitioning  : PartitioningSubmitDTO to be used to ensure server knows the given partitioning.
+   *  @return AtumContextDTO.
    */
   override def createPartitioning(partitioning: PartitioningSubmitDTO): AtumContextDTO = {
     ts.put(createPath(partitioning.partitioning), Events(Nil, Map.empty))
@@ -26,25 +25,57 @@ class Capture(maxEvents: Int) extends Dispatcher {
   }
 
   /**
-   * This method is used to save checkpoint to server.
+   *  This method is used to save checkpoint to server.
    *
-   * @param checkpoint : CheckpointDTO to be saved.
+   *  @param checkpoint : CheckpointDTO to be saved.
    */
   override def saveCheckpoint(checkpoint: CheckpointDTO): Unit = {
-    val events = capture.getOrElse(checkpoint.partitioning, Events(Nil, Map.empty))
-    val newEvents = events.copy(checkpoint = checkpoint :: events.checkpoint.take(maxEvents - 1))
-    capture + (checkpoint.partitioning -> newEvents)
+    ts.compute(
+      createPath(checkpoint.partitioning),
+      (_, events) =>
+        Option(events)
+          .map(_.copy(checkpoint = checkpoint :: events.checkpoint.take(maxEvents - 1)))
+          .getOrElse(Events(Nil, Map.empty))
+    )
   }
 
   /**
-   * This method is used to save the additional data to the server.
+   *  This method is used to save the additional data to the server.
    *
-   * @param additionalData the data to be saved.
+   *  @param additionalData the data to be saved.
    */
   override def saveAdditionalData(additionalData: AdditionalDataSubmitDTO): Unit = {
-    val events = capture.getOrElse(additionalData.partitioning, Events(Nil, Map.empty))
-    val newEvents = events.copy(additionalData = additionalData.additionalData)
-    capture + (additionalData.partitioning -> newEvents)
+    ts.compute(
+      createPath(additionalData.partitioning),
+      (_, events) =>
+        Option(events).map(_.copy(additionalData = additionalData.additionalData)).getOrElse(Events(Nil, Map.empty))
+    )
+  }
+
+  def prefixIter(prefix: String): Iterator[(String, Events)] = {
+    val prefixWithSlash = sanitizePrefix(prefix)
+    val prefixWithSlashLength = prefixWithSlash.length
+    val tailMap = ts.tailMap(prefixWithSlash)
+    val it = tailMap.entrySet().iterator()
+    new Iterator[(String, Events)] {
+      private var _prefetched = Option.empty[Events]
+      override def hasNext: Boolean = _prefetched match {
+        case Some(_) => true
+        case None if it.hasNext =>
+          val entry = it.next()
+          val key = entry.getKey
+          if (key.startsWith(prefixWithSlash)) {
+            _prefetched = Some(entry.getValue)
+            true
+          } else false
+      }
+      override def next(): (String, Events) = {
+        val entry = it.next()
+        val key = entry.getKey
+        val keyWithoutPrefix = key.substring(prefixWithSlashLength)
+        keyWithoutPrefix -> entry.getValue
+      }
+    }
   }
 }
 
@@ -55,4 +86,12 @@ object Capture {
     checkpoint: List[CheckpointDTO],
     additionalData: Map[String, Option[String]]
   )
+
+  private def sanitizePrefix(prefix: String): String = {
+    if (prefix == null || prefix.isEmpty) "/"
+    else {
+      val withLeadingSlash = if (prefix.startsWith("/")) prefix else s"/$prefix"
+      if (withLeadingSlash.endsWith("/")) withLeadingSlash else s"$withLeadingSlash/"
+    }
+  }
 }
