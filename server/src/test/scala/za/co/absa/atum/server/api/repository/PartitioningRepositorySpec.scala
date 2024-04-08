a1c6cca8208fb710948663179c473a1468b4c79c
AB019TC <tebalelo.sekhula@absa.africa>
2024-04-08 13:51:05 +0200

    Fixes #120 - fixing type mismatch in PartitioningRepositorySpec

--- server/src/test/scala/za/co/absa/atum/server/api/repository/PartitioningRepositorySpec.scala
+++ server/src/test/scala/za/co/absa/atum/server/api/repository/PartitioningRepositorySpec.scala
@@ -18,7 +18,11 @@ package za.co.absa.atum.server.api.repository
 
 import org.junit.runner.RunWith
 import org.mockito.Mockito.{mock, when}
-import za.co.absa.atum.server.api.database.runs.functions.{CreateOrUpdateAdditionalData, CreatePartitioningIfNotExists, GetPartitioningAdditionalData, GetPartitioningMeasures}
+import za.co.absa.atum.server.api.database.runs.functions.{
+  CreateOrUpdateAdditionalData,
+  CreatePartitioningIfNotExists,
+  GetPartitioningAdditionalData,
+  GetPartitioningMeasures}
 import za.co.absa.atum.server.api.exception.DatabaseError
 import za.co.absa.atum.server.api.TestData
 import za.co.absa.fadb.exceptions.ErrorInDataException
@@ -112,9 +116,9 @@ class PartitioningRepositorySpec extends ZIOSpecDefault with TestData {
     ).provide(
       PartitioningRepositoryImpl.layer,
       createPartitioningIfNotExistsMockLayer,
-      createOrUpdateAdditionalDataMockLayer,
       getPartitioningMeasuresMockLayer,
-      getPartitioningAdditionalDataMockLayer
+      getPartitioningAdditionalDataMockLayer,
+      createOrUpdateAdditionalDataMockLayer
     )
 
   }
