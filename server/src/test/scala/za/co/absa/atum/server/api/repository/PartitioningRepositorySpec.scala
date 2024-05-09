0ae952195fce3222d76f9c847130d20e3209f740
AB019TC <tebalelo.sekhula@absa.africa>
2024-05-09 10:09:14 +0200

    Fixes #190 - adding mock layer from PartitioningRepositorySpec

--- server/src/test/scala/za/co/absa/atum/server/api/repository/PartitioningRepositorySpec.scala
+++ server/src/test/scala/za/co/absa/atum/server/api/repository/PartitioningRepositorySpec.scala
@@ -19,12 +19,7 @@ package za.co.absa.atum.server.api.repository
 import org.junit.runner.RunWith
 import org.mockito.Mockito.{mock, when}
 import za.co.absa.atum.model.dto.{AdditionalDataDTO, MeasureDTO}
-import za.co.absa.atum.server.api.database.runs.functions.{
-  CreateOrUpdateAdditionalData,
-  CreatePartitioningIfNotExists,
-  GetPartitioningAdditionalData,
-  GetPartitioningMeasures
-}
+import za.co.absa.atum.server.api.database.runs.functions.{CreateOrUpdateAdditionalData, CreatePartitioningIfNotExists, GetPartitioningAdditionalData, GetPartitioningCheckpoints, GetPartitioningMeasures}
 import za.co.absa.atum.server.api.exception.DatabaseError
 import za.co.absa.atum.server.api.TestData
 import za.co.absa.fadb.exceptions.ErrorInDataException
@@ -75,6 +70,12 @@ class PartitioningRepositorySpec extends ZIOSpecDefault with TestData {
   when(getPartitioningAdditionalDataMock.apply(partitioningDTO2)).thenReturn(ZIO.fail(DatabaseError("boom!")))
 
   private val getPartitioningAdditionalDataMockLayer = ZLayer.succeed(getPartitioningAdditionalDataMock)
+
+  // Get Partitioning Checkpoints Mocks
+  private val getPartitioningCheckpointsMock = mock(classOf[GetPartitioningCheckpoints])
+
+  private val getPartitioningCheckpointsMockLayer = ZLayer.succeed(getPartitioningCheckpointsMock)
+
 
   override def spec: Spec[TestEnvironment with Scope, Any] = {
 
@@ -145,7 +146,8 @@ class PartitioningRepositorySpec extends ZIOSpecDefault with TestData {
       createPartitioningIfNotExistsMockLayer,
       getPartitioningMeasuresMockLayer,
       getPartitioningAdditionalDataMockLayer,
-      createOrUpdateAdditionalDataMockLayer
+      createOrUpdateAdditionalDataMockLayer,
+      getPartitioningCheckpointsMockLayer
     )
 
   }
