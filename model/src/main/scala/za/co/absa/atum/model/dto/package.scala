a85e6728b4fad68837bd9a53ce45cb4069d73ee6
AB019TC <tebalelo.sekhula@absa.africa>
2024-07-11 22:38:29 +0200

    Implicit encoders and decoders for PartitioningDTO

--- model/src/main/scala/za/co/absa/atum/model/dto/package.scala
+++ model/src/main/scala/za/co/absa/atum/model/dto/package.scala
@@ -28,4 +28,7 @@ package object dto {
   implicit val decodeAdditionalDataDTO: Decoder[AdditionalDataDTO] = Decoder.decodeMap[String, Option[String]]
   implicit val encodeAdditionalDataDTO: Encoder[AdditionalDataDTO] = Encoder.encodeMap[String, Option[String]]
 
+  // Implicit encoders and decoders for PartitioningDTO
+  implicit val decodePartitioningDTO: Decoder[PartitioningDTO] = Decoder.decodeSeq[PartitionDTO]
+  implicit val encodePartitioningDTO: Encoder[PartitioningDTO] = Encoder.encodeSeq[PartitionDTO]
 }
