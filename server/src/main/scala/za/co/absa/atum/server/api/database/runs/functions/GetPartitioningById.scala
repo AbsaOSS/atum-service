ee434e3bdd657fa807bfdf91538878b98965c50b
AB019TC <tebalelo.sekhula@absa.africa>
2024-08-06 19:42:13 +0200

    creating GetPartitioningById class

---
+++ server/src/main/scala/za/co/absa/atum/server/api/database/runs/functions/GetPartitioningById.scala
@@ -0,0 +1,28 @@
+package za.co.absa.atum.server.api.database.runs.functions
+
+import doobie.implicits.toPutOps
+import io.circe.Json
+import za.co.absa.atum.server.api.database.PostgresDatabaseProvider
+import za.co.absa.atum.server.api.database.runs.Runs
+import za.co.absa.db.fadb.DBSchema
+import za.co.absa.db.fadb.doobie.DoobieEngine
+import za.co.absa.db.fadb.doobie.DoobieFunction.DoobieSingleResultFunctionWithStatus
+import za.co.absa.db.fadb.status.handling.implementations.StandardStatusHandling
+import zio._
+import za.co.absa.atum.server.api.database.DoobieImplicits.Sequence.get
+import doobie.postgres.implicits._
+import za.co.absa.db.fadb.doobie.postgres.circe.implicits.jsonbGet
+
+class GetPartitioningById(implicit schema: DBSchema, dbEngine: DoobieEngine[Task]) extends
+  DoobieSingleResultFunctionWithStatus[Long, Json, Task](values => fr"$values") with StandardStatusHandling {
+
+  override def fieldsToSelect: Seq[String] = super.fieldsToSelect ++ Seq("partitioning")
+}
+
+object GetPartitioningById {
+  val layer: URLayer[PostgresDatabaseProvider, GetPartitioningById] = ZLayer {
+    for {
+      dbProvider <- ZIO.service[PostgresDatabaseProvider]
+    } yield new GetPartitioningById()(Runs, dbProvider.dbEngine)
+  }
+}
