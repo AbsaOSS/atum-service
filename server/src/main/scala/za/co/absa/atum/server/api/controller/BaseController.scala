68ee4b2833d5bd89fa28a7f0c1f32f880200376e
Pavel Salamon <salamonpavel@gmail.com>
2024-08-12 15:27:45 +0200

    tests

--- server/src/main/scala/za/co/absa/atum/server/api/controller/BaseController.scala
+++ server/src/main/scala/za/co/absa/atum/server/api/controller/BaseController.scala
@@ -19,8 +19,8 @@ package za.co.absa.atum.server.api.controller
 import za.co.absa.atum.server.api.exception.ServiceError
 import za.co.absa.atum.server.api.exception.ServiceError._
 import za.co.absa.atum.server.api.http.ApiPaths
-import za.co.absa.atum.server.model.{ConflictErrorResponse, ErrorResponse, InternalServerErrorResponse, NotFoundErrorResponse}
-import za.co.absa.atum.server.model.SuccessResponse.{MultiSuccessResponse, SingleSuccessResponse}
+import za.co.absa.atum.server.model._
+import za.co.absa.atum.server.model.SuccessResponse._
 import zio._
 
 trait BaseController {
