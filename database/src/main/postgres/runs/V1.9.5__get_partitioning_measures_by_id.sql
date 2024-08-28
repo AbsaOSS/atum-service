a95ed412dffe8c45c88823b30fb0d409c6a4b9ef
AB019TC <tebalelo.sekhula@absa.africa>
2024-08-28 09:26:12 +0200

    restoring db-function

--- database/src/main/postgres/runs/V1.9.5__get_partitioning_measures_by_id.sql
+++ database/src/main/postgres/runs/V1.9.5__get_partitioning_measures_by_id.sql
@@ -13,3 +13,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+CREATE OR REPLACE FUNCTION runs.get_partitioning_measures_by_id(
+    IN i_partitioning_id          BIGINT,
+    OUT status                    INTEGER,
+    OUT status_text               TEXT,
+    OUT measure_name              TEXT,
+    OUT measured_columns          TEXT[]
+) RETURNS SETOF record AS
+$$
+-------------------------------------------------------------------------------
+--
+-- Function: runs.get_partitioning_measures_by_id(1)
+--      Returns measures for the given partitioning id
+--
+-- Parameters:
+--      i_partitioning_id       - partitioning id we are asking the measures for
+--
+-- Returns:
+--      status                  - Status code
+--      status_text             - Status message
+--      measure_name            - Name of the measure
+--      measured_columns        - Array of columns associated with the measure
+--
+-- Status codes:
+--      11 - OK
+--      41 - Partitioning not found
+--      42 - Measures not found
+--
+-------------------------------------------------------------------------------
+BEGIN
+
+    PERFORM 1 FROM runs.partitionings WHERE id_partitioning = i_partitioning_id;
+
+    IF NOT FOUND THEN
+        status := 41;
+        status_text := 'Partitioning not found';
+        RETURN NEXT;
+        RETURN;
+    END IF;
+
+    RETURN QUERY
+        SELECT MD.measure_name, MD.measured_columns
+        FROM runs.measure_definitions AS MD
+        WHERE MD.fk_partitioning = i_partitioning_id;
+
+    IF FOUND THEN
+        status := 11;
+        status_text := 'Measures found';
+    ELSE
+        status := 42;
+        status_text := 'Measures not found';
+    END IF;
+
+    RETURN NEXT;
+    RETURN;
+
+END;
+$$
+LANGUAGE plpgsql volatile SECURITY DEFINER;
+
+ALTER FUNCTION runs.get_partitioning_measures_by_id(BIGINT) OWNER TO atum_owner;
+GRANT EXECUTE ON FUNCTION runs.get_partitioning_measures_by_id(BIGINT) TO atum_user;
