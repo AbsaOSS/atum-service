e9b2566adcdfbaf3212c5c748a8376be04256e0a
Ladislav Sulak <laco.sulak@gmail.com>
2024-09-16 13:45:54 +0200

    #234: DB function for GET main flow of a partitioning, including Balta tests

---
+++ database/src/main/postgres/runs/V1.9.9__get_partitioning_main_flow.sql
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2021 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CREATE OR REPLACE FUNCTION runs.get_partitioning_main_flow(
+    IN i_partitioning_id BIGINT,
+    OUT status INTEGER,
+    OUT status_text TEXT,
+    OUT id_flow BIGINT,
+    OUT flow_name TEXT,
+    OUT flow_description TEXT,
+    OUT from_pattern BOOLEAN
+) RETURNS record AS
+$$
+    -------------------------------------------------------------------------------
+--
+-- Function: runs.get_partitioning_main_flow(1)
+--      Returns the main flow for the given partitioning
+--
+-- Parameters:
+--      i_partitioning_id   - id of the partitioning for requested main flow
+--
+-- Returns:
+--      status              - Status code
+--      status_text         - Status message
+--      id_flow             - ID of the main flow associated with a given partitioning
+--      flow_name           - Name of the main flow
+--      flow_description    - Description of the main flow
+--      from_pattern        - Flag if the main flow was generated from pattern
+--
+-- Status codes:
+--      11 - OK
+--      41 - Partitioning not found
+--      50 - Main flow not found
+--
+-------------------------------------------------------------------------------
+
+BEGIN
+    PERFORM 1 FROM runs.partitionings WHERE id_partitioning = i_partitioning_id;
+    IF NOT FOUND THEN
+        status := 41;
+        status_text := 'Partitioning not found';
+        RETURN;
+    END IF;
+
+    status = 11;
+    status_text = 'OK';
+
+    SELECT F.id_flow,
+           F.flow_name,
+           F.flow_description,
+           F.from_pattern
+    FROM flows.flows AS F
+    WHERE F.fk_primary_partitioning = i_partitioning_id
+    INTO id_flow, flow_name, flow_description, from_pattern;
+
+    IF NOT FOUND THEN
+        status := 50;
+        status_text := 'Main flow not found';
+        RETURN;
+    END IF;
+END;
+$$
+LANGUAGE plpgsql VOLATILE SECURITY DEFINER;
+
+ALTER FUNCTION runs.get_partitioning_main_flow(BIGINT) OWNER TO atum_owner;
+GRANT EXECUTE ON FUNCTION runs.get_partitioning_main_flow(BIGINT) TO atum_user;
---
+++ database/src/test/scala/za/co/absa/atum/database/runs/GetPartitioningMainFlowIntegrationTests.scala
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2021 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.atum.database.runs
+
+import za.co.absa.balta.DBTestSuite
+import za.co.absa.balta.classes.JsonBString
+
+class GetPartitioningMainFlowIntegrationTests extends DBTestSuite {
+
+  private val fncGetPartitioningMainFlow = "runs.get_partitioning_main_flow"
+
+  private val partitioning1 = JsonBString(
+    """
+      |{
+      |   "version": 1,
+      |   "keys": ["keyX", "keyY", "keyZ"],
+      |   "keysToValues": {
+      |     "keyX": "value1",
+      |     "keyZ": "value3",
+      |     "keyY": "value2"
+      |   }
+      |}
+      |""".stripMargin
+  )
+
+  private val partitioning2 = JsonBString(
+    """
+      |{
+      |  "version": 1,
+      |  "keys": ["key1", "key3", "key2", "key4"],
+      |  "keysToValues": {
+      |    "key1": "valueX",
+      |    "key2": "valueY",
+      |    "key3": "valueZ",
+      |    "key4": "valueA"
+      |  }
+      |}
+      |""".stripMargin
+  )
+
+  test("Get partitioning main flow returns main flow for partitioning with such data") {
+    table("runs.partitionings").insert(
+      add("partitioning", partitioning1)
+        .add("created_by", "Sirius")
+    )
+
+    table("runs.partitionings").insert(
+      add("partitioning", partitioning2)
+        .add("created_by", "Snape")
+    )
+
+    val fkPartitioning1: Long = table("runs.partitionings").fieldValue("partitioning", partitioning1, "id_partitioning").get.get
+    val fkPartitioning2: Long = table("runs.partitionings").fieldValue("partitioning", partitioning2, "id_partitioning").get.get
+
+    table("flows.partitioning_to_flow").insert(
+      add("fk_flow", 1L)
+        .add("fk_partitioning", fkPartitioning1)
+        .add("created_by", "Sirius")
+    )
+
+    table("flows.partitioning_to_flow").insert(
+      add("fk_flow", 2L)
+        .add("fk_partitioning", fkPartitioning2)
+        .add("created_by", "Snape")
+    )
+
+    table("flows.flows").insert(
+      add("id_flow", 1L)
+        .add("flow_name", "Flow1")
+        .add("flow_description", "test flow 1")
+        .add("from_pattern", false)
+        .add("created_by", "Sirius")
+        .add("fk_primary_partitioning", fkPartitioning1)
+    )
+
+    table("flows.flows").insert(
+      add("id_flow", 2L)
+        .add("flow_name", "Flow2")
+        .add("flow_description", "test flow 2")
+        .add("from_pattern", true)
+        .add("created_by", "Snape")
+        .add("fk_primary_partitioning", fkPartitioning2)
+    )
+
+    function(fncGetPartitioningMainFlow)
+      .setParam("i_partitioning_id", fkPartitioning1)
+      .execute { queryResult =>
+        val results = queryResult.next()
+        assert(results.getLong("id_flow").contains(1L))
+        assert(results.getString("flow_name").contains("Flow1"))
+        assert(results.getString("flow_description").contains("test flow 1"))
+        assert(results.getBoolean("from_pattern").contains(false))
+
+        assert(!queryResult.hasNext)
+      }
+
+    function(fncGetPartitioningMainFlow)
+      .setParam("i_partitioning_id", fkPartitioning2)
+      .execute { queryResult =>
+        val results = queryResult.next()
+        assert(results.getLong("id_flow").contains(2L))
+        assert(results.getString("flow_name").contains("Flow2"))
+        assert(results.getString("flow_description").contains("test flow 2"))
+        assert(results.getBoolean("from_pattern").contains(true))
+
+        assert(!queryResult.hasNext)
+      }
+
+    table("flows.flows").where(add("fk_primary_partitioning", fkPartitioning1)) { queryResult =>
+      assert(queryResult.hasNext)
+
+      val row = queryResult.next()
+      assert(row.getLong("id_flow").contains(1L))
+      assert(row.getString("flow_name").contains("Flow1"))
+      assert(row.getString("created_by").contains("Sirius"))
+
+      assert(!queryResult.hasNext)
+    }
+
+    table("flows.flows").where(add("fk_primary_partitioning", fkPartitioning2)) { queryResult =>
+      assert(queryResult.hasNext)
+
+      val row = queryResult.next()
+      assert(row.getLong("id_flow").contains(2L))
+      assert(row.getString("flow_name").contains("Flow2"))
+      assert(row.getString("created_by").contains("Snape"))
+
+      assert(!queryResult.hasNext)
+    }
+
+  }
+
+  test("Get partitioning main flow should return error for partitioning with no main flow (not typical/valid scenario)") {
+    table("runs.partitionings").insert(
+      add("partitioning", partitioning2)
+        .add("created_by", "Dobby")
+    )
+
+    val fkPartitioning: Long = table("runs.partitionings").fieldValue("partitioning", partitioning2, "id_partitioning").get.get
+
+    function(fncGetPartitioningMainFlow)
+      .setParam("i_partitioning_id", fkPartitioning)
+      .execute { queryResult =>
+        val result = queryResult.next()
+        assert(result.getInt("status").contains(50))
+        assert(result.getString("status_text").contains("Main flow not found"))
+        assert(!queryResult.hasNext)
+      }
+
+    table("runs.additional_data").where(add("fk_partitioning", fkPartitioning)) { additionalDataResult =>
+      assert(!additionalDataResult.hasNext)
+    }
+  }
+
+  test("Get partitioning main flow should return error status code on non existing partitioning") {
+    function(fncGetPartitioningMainFlow)
+      .setParam("i_partitioning_id", 0L)
+      .execute { queryResult =>
+        val results = queryResult.next()
+        assert(results.getInt("status").contains(41))
+        assert(results.getString("status_text").contains("Partitioning not found"))
+        assert(!queryResult.hasNext)
+      }
+  }
+
+}
