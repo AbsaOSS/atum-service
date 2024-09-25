9bf1d9294216c1cc62ae1c778f92c6f8cdc7a937
Ladislav Sulak <laco.sulak@gmail.com>
2024-09-25 20:43:44 +0200

    refactoring

--- server/docker_entrypoint.sh
+++ server/docker_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # These two are locations known and used in the Dockerfile.
-CONFIG_LOCATION="/opt/config/resource.conf"
+CUSTOM_CONFIG_LOCATION="/opt/config/resource.conf"
 JAR_LOCATION="/opt/app/atum-service.jar"
 
-if [ -f "${CONFIG_LOCATION}" ]; then
+if [ -f "${CUSTOM_CONFIG_LOCATION}" ]; then
     echo "Running with custom config"
-    java -Dconfig.file="${CONFIG_LOCATION}" -jar "${JAR_LOCATION}"
+    java -Dconfig.file="${CUSTOM_CONFIG_LOCATION}" -jar "${JAR_LOCATION}"
 else
     echo "Running with default config"
     java -jar "${JAR_LOCATION}"
