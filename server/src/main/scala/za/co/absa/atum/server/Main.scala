e9ba513ff0f526e6f419a27e9b1102b8b37c6e4d
Tobias.Mikula <tobias.mikula@absa.africa>
2026-06-22 13:45:59 +0200

    Triggering the JaCoCo report.

--- server/src/main/scala/za/co/absa/atum/server/Main.scala
+++ server/src/main/scala/za/co/absa/atum/server/Main.scala
@@ -70,7 +70,7 @@ object Main extends ZIOAppDefault {
     ZIO.config[JvmMonitoringConfig](JvmMonitoringConfig.config).flatMap { jvmMonitoringConfig =>
       Server.server
         .provide(
-          // controllers
+          // controller
           PartitioningControllerImplV1.layer,
           PartitioningControllerImplV2.layer,
           CheckpointControllerImplV1.layer,
